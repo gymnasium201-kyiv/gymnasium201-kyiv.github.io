c8940e3e3ecf183106fa7593d4e2a5b47f0f7e28
WexSeven <205093375+1test-gymnasium201-kyiv@users.noreply.github.com>
2025-10-05 23:33:21 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://1test-gymnasium201-kyiv.github.io',
+  site: 'https://gymnasium201-kyiv.github.io',
   integrations: [tailwind()]
 })
